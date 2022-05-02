ba6849060deb58368ba32ee8db654bf565470370
nadd2031 <naddiamary@gamil.com>
2022-05-02 16:48:09 -0300

    Se adjunto el boton para editar acerca

--- frontend/src/app/componentes/acerca/acerca.component.ts
+++ frontend/src/app/componentes/acerca/acerca.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MiportfolioService } from 'src/app/servicios/miportfolio.service';
 
 @Component({
@@ -8,14 +9,35 @@ import { MiportfolioService } from 'src/app/servicios/miportfolio.service';
 })
 export class AcercaComponent implements OnInit {
    acerca:any;
-  constructor(private miServicio:MiportfolioService) { }
-
+   usuarioAutenticado:boolean=true;
+   form:FormGroup;
+  constructor(private miServicio:MiportfolioService,private miFormBuilder:FormBuilder) {
+    this.form=this.miFormBuilder.group({
+      school:['',[Validators.required,Validators.minLength(5)]],
+      title:['',[Validators.required,Validators.minLength(5)]],
+      img:['https://',[Validators.required,Validators.pattern('https?://.+')]],
+    })
+  }
+  get school(){
+    return this.form.get("school");
+  }
   ngOnInit(): void {
     this.miServicio.obtenerDatosAcerca().subscribe(data=>{
       console.log(data);
       this.acerca=data["educacion"];
     });
   }
-
+  guardarAcerca(){
+    if(this.form.valid){
+     alert("Enviar los datos al servicio (servidor)");
+     this.form.reset();
+     document.getElementById("cerrarModalAcerca")?.click();
+    }
+    else
+    {
+      alert("Hay errores");
+      this.form.markAllAsTouched();
+    }
+ }
 }
 
